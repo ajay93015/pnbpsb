a4f5d8c0541c45e29125140777f07e62f75baf26
ajay93015 <105852839+ajay93015@users.noreply.github.com>
2025-07-11 21:52:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+const app = express();
 const port = 3000;
 
 
